681671282ba0b4a9e6a4888ca925b6ed92c07bc7
UmerYou <umeryou@outlook.com>
2024-12-04 10:38:57 +0500

    resolve conflict

--- test-client/client/src/components/AllBanks.jsx
+++ test-client/client/src/components/AllBanks.jsx
@@ -41,74 +41,6 @@ const AllBanks = () => {
               <span className="line"></span>
               <span className="text">LET'S DISCOVER BY BANKS</span>
               <span className="line"></span>
-<<<<<<< HEAD
-            </div>
-            <div className="container">
-              <div className="row">
-                {banksToShow.map((bank, index) => (
-                  <div
-                    className={`col-md-4 fade-in ${
-                      loadingMore ? "loading" : ""
-                    }`} // Apply animation class conditionally
-                    key={bank.id}
-                    style={{ animationDelay: `${index * 0.1}s` }} // Stagger animation
-                  >
-                    <BankCard bank={bank} />
-                  </div>
-                ))}
-              </div>
-              {banksToShow.length < allbanks.length && ( // Show button if more banks are available
-                <div className="text-center mt-4">
-                  <button className="btn btn-primary" onClick={loadMore}>
-                    {loadingMore ? "Loading..." : "Load More"}
-                  </button>
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-    useEffect(() => {
-        const getAllBanks = async () => {
-            try {
-                const data = await fetchallBanks();
-                console.log(data);
-                setBanks(data);
-            } catch (error) {
-                console.error("Error fetching Banks:", error);
-            }
-        };
-        getAllBanks();
-    }, []);
-    return (
-        <>
-            <div className="container-fluid mt-5 bank_bg">
-                <div className="row">
-                    <div className="col-lg-12 col-sm">
-                        <h1 className="main_heading_bank_landing pt-5">Banks</h1>
-                        <div class="side_border_dots_bank_landing pt-3 pb-5">
-                            <span class="line"></span>
-                            <span class="text">LET'S DISCOVER BY BANKS</span>
-                            <span class="line"></span>
-                        </div>
-                        <div className="container">
-                            <div className="row">
-                                {allbanks.map((bank) => (
-                                    <div className="col-md-3" key={bank.id}>
-                                        <BankCard bank={bank} />
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </>
-    );
-=======
             </div>
             <div className="container">
               <div className="row">
@@ -137,7 +69,6 @@ const AllBanks = () => {
       </div>
     </>
   );
->>>>>>> uidev
 };
 
 export default AllBanks;
