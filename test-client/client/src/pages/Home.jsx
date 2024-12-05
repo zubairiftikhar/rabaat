b2a4c382bbc90c7a92263c806fdefc9eb3bce126
UmerYou <umeryou@outlook.com>
2024-12-05 16:35:06 +0500

    main hero section for home

--- test-client/client/src/pages/Home.jsx
+++ test-client/client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 // src/pages/Home.jsx
 import React from "react";
 import AllCities from "../components/AllCities";
+import HeroSection from "../components/HeroSection";
 
 const Home = () => {
   return (
     <>
+    <HeroSection/>
       <AllCities />
     </>
   );
