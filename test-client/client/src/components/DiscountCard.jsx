d2e6c94243bfc395609363c19686cfbb9c4c28be
UmerYou <umeryou@outlook.com>
2024-12-10 11:01:11 +0500

    take pull by auth user

--- test-client/client/src/components/DiscountCard.jsx
+++ test-client/client/src/components/DiscountCard.jsx
@@ -4,19 +4,19 @@ import "../css/branchdiscount.css";
 
 const DiscountCard = ({ discount }) => {
   return (
-    <div className="card shadow-sm border-0 rounded-3 h-100">
-      <div className="card-header bg-primary text-white">
+    <div className="card shadow-sm card_header_discount_main rounded-4 h-100">
+      <div className="card-header card_header_discount text-white">
         <h5 className="card-title">{discount.percentage}% Off</h5>
       </div>
       <div className="card-body">
         <p className="card-text">
-          Applicable Cards:
+          <span>Applicable Cards:</span>
           <ul>
             {discount.cards.map((card, index) => (
               <li key={index}>
                 <div className="card-image mb-3">
                   <img
-                    src={discount.card_image}
+                    src={`/src/assets/img/cards/hbl${discount.card_image}`}
                     alt="Card"
                     className="img-fluid"
                     style={{ maxWidth: "80px", borderRadius: "8px" }}
