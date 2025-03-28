a4d819d4d58011fad4df8ec08510dd7eb0f7e042
UmerYou <umeryou@outlook.com>
2025-03-28 14:55:16 +0500

    take pull from main

--- test-client/client/src/pages/Merchants.jsx
+++ test-client/client/src/pages/Merchants.jsx
@@ -181,7 +181,7 @@ const Merchants = () => {
                 {filteredMerchants.slice(0, visibleMerchants).map((merchant) => (
                   <div
                     key={merchant.id}
-                    className="col-lg-2 col-md-4 col-4 merchant-card-spacing"
+                    className="col-lg-2 col-md-4 col-sm-4 merchant-card-spacing"
                   >
                     <MerchantCard
                       cityName={cityName}
