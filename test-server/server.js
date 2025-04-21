6e26080343f9864931cced49fe62aa7f2562f3ae
Syed Wasi Ul Hassan <wasishah8283@gmail.com>
2025-04-21 11:29:31 +0500

    sitemap updated

--- test-server/server.js
+++ test-server/server.js
@@ -7,8 +7,8 @@ const jwt = require("jsonwebtoken");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const sitemapRoutes = require("./sitemap");
-app.use("/", sitemapRoutes);
+// const sitemapRoutes = require("./sitemap");
+// app.use("/", sitemapRoutes);
 
 
 // Database Connection
